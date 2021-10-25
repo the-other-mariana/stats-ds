1f05a6ce3fc18cca7f262420eaea177391a6c14c
the-other-mariana <mariana.avalos.arce@gmail.com>
2021-10-25 08:58:22 -0500

    week 8 code
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

---
+++ week8/session-08.R
@@ -0,0 +1,71 @@
+# histogram of relative cumulative function
+
+mydata = faithful
+
+# exercise: plot the cumulative relative frequency of eruptions column
+# option 1: using hist to get counts (absolute frequency)
+erup = mydata$eruptions
+str(erup)
+min(erup) # 1.4
+max(erup) # 5.6
+breaks1 = seq(1, 6, by=0.1)
+erupt = hist(erup, breaks=breaks1, right=FALSE)
+reldata = erupt$counts / nrow(mydata)
+min(reldata)
+max(reldata)
+cumfreq = cumsum(reldata)
+#plot(cumfreq, main='Eruptions', xlab='Minutes', ylab='Cumulative Relative Frecuency')
+#lines(cumfreq)
+
+# option 2: using cut and table to get counts (absolute frequency)
+eruptions = mydata$eruptions
+breaks2 = seq(1,6, by =0.1)
+eruptions_cut = cut(eruptions, breaks2, right=F)
+eruptions_table = table(eruptions_cut)
+relfreq = eruptions_table / nrow(mydata)
+cumrelfreq = cumsum(relfreq)
+plot(cumrelfreq, main='Eruptions', xlab='Minutes', ylab='Cumulative Relative Frecuency')
+lines(cumrelfreq)
+
+# additional: cumulative absolute frequency
+eruptions = mydata$eruptions
+breaks3 = seq(1, 6, by = 0.1)
+eruptions_cut = cut(eruptions, breaks3, right=F)
+eruptions_table = table(eruptions_cut) # abs freq
+cumabsfreq = cumsum(eruptions_table)
+#plot(cumabsfreq, main='Eruptions', xlab='Minutes', ylab='Cumulative Absolute Frecuency')
+#lines(cumabsfreq)
+
+# additional: absolute frequency bar plot
+eruptions = mydata$eruptions
+breaks4 = seq(1,6, by=0.1)
+eruptions_cut = cut(eruptions, breaks4, right=F)
+eruptions_table = table(eruptions_cut)
+#barplot(eruptions_table, main='Eruptions')
+
+# additional: relative frequency bar plot
+eruptions = mydata$eruptions
+breaks4 = seq(1,6, by=0.1)
+eruptions_cut = cut(eruptions, breaks4, right=F)
+eruptions_table = table(eruptions_cut)
+relfreq = eruptions_table / nrow(mydata)
+#barplot(relfreq, main='Eruptions')
+
+# pie chart 
+# pie(eruptions_table)
+
+# stem and leaf 
+stem(eruptions)
+
+# stdev
+x = mydata$eruptions
+mean_erup = mean(x)
+variance = sum((x - mean_erup)^2) / (length(x))
+stdev = sd(x)
+dev_coeff = stdev / mean_erup
+
+mean_erup
+stdev
+dev_coeff
+stem(x)
+# the number 34 is 3.4
